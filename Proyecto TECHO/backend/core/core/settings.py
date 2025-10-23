e13eaf23fd1671d279d5ca02ef4261ccd09513aa
Ricardo Flores <ric.flores@duocuc.cl>
2025-10-23 12:47:28 -0300

    Proyecto TECHO dev v_002

---
+++ Proyecto TECHO/backend/core/core/settings.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR.parent / ".env")
+
+SECRET_KEY = os.getenv("DJANGO_SECRET", "dev-secret")
+DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+INSTALLED_APPS = [
+  "django.contrib.admin","django.contrib.auth","django.contrib.contenttypes",
+  "django.contrib.sessions","django.contrib.messages","django.contrib.staticfiles",
+  "rest_framework","corsheaders","api",
+]
+
+MIDDLEWARE = [
+  "corsheaders.middleware.CorsMiddleware",
+  "django.middleware.security.SecurityMiddleware",
+  "django.contrib.sessions.middleware.SessionMiddleware",
+  "django.middleware.common.CommonMiddleware",
+  "django.middleware.csrf.CsrfViewMiddleware",
+  "django.contrib.auth.middleware.AuthenticationMiddleware",
+  "django.contrib.messages.middleware.MessageMiddleware",
+  "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+# permitir que el FRONT (servido por Live Server) consuma la API
+CORS_ALLOWED_ORIGINS = [
+  "http://127.0.0.1:5500",
+  "http://localhost:5500",
+]
+
+REST_FRAMEWORK = {
+  "DEFAULT_AUTHENTICATION_CLASSES": (
+    "rest_framework_simplejwt.authentication.JWTAuthentication",
+  )
+}
+
+# PostgreSQL (usa tu DB validada)
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "5432"),
+    }
+}
+
+STATIC_URL = "static/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR.parent / "media"
